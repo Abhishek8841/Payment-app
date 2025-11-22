132609c89a5e6bbabe4559226b9a0b938c8b5954
Abhishek8841 <abhishekbatra2005@gmail.com>
2025-11-22 20:02:11 +0530

    Fixed some bugs

--- Backend/config/database.js
+++ Backend/config/database.js
@@ -2,14 +2,9 @@ const mongoose = require("mongoose")
 require("dotenv").config();
 
 exports.dbConnect = () => {
-    mongoose.connect(process.env.DATABASE_URL,
-        {
-            useUnifiedTopology: true,
-            useNewUrlParser: true,
-        }
-    )
+    mongoose.connect(process.env.DATABASE_URL)
         .then(() => { console.log("Database is connected") })
-        .catch(() => {
+        .catch((error) => {
             console.log("Database connection error", error);
             process.exit(1);
         })
